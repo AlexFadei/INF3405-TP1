f4416787c64601783de9b360a0ef4c6a4bf3ddc6
AlexFadei <alefad16@gmail.com>
2025-10-03 10:12:24 -0400

    implemented commands server side except upload and download

--- src/Server/ClientHandler.java
+++ src/Server/ClientHandler.java
@@ -2,6 +2,7 @@ package Server;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -11,6 +12,7 @@ import LoggerUtil.LoggerUtil;
 
 //worker thread for the client
 public class ClientHandler extends Thread {
+	private File currentFileDirectory;
 	private Socket socket; private int clientNumber;
 	private boolean exitSocket;
 	
@@ -18,42 +20,109 @@ public class ClientHandler extends Thread {
 		this.socket = socket;
 		this.clientNumber = clientNumber; System.out.println("New connection with client#" + clientNumber + " at" + socket);
 		this.exitSocket = false;
+		this.currentFileDirectory = new File(System.getProperty("user.dir"));
+		
 	}
 	public void ExecuteCommand(String command) {
-
 		String[] parts = command.split(" ", 2);
-		switch(parts[0]) {
+		String cmd = parts[0];
+		String arg = parts.length > 1 ? parts[1] : "";
+		LoggerUtil.log(this.socket, command);
+		
+		switch(cmd) {
 				
-		case "ls":
-			LoggerUtil.log(this.socket, command);
-			break;
+		case "ls": handleLs(); break;
 			
-		case "cd":
-			LoggerUtil.log(this.socket, command);
-			break;
+		case "cd": handleCd(arg); break;
 			
-		case "mkdir":
-			LoggerUtil.log(this.socket, command);
-			break;
+		case "mkdir": handleMkDir(arg); break;
 			
-		case "upload":
-			LoggerUtil.log(this.socket, command);
-			break;
+		case "upload": handleUpload(arg); break;
 			
-		case "download":
-			LoggerUtil.log(this.socket, command);
-			break;
+		case "download": handleDownload(arg); break;
 			
-		case "delete":
-			LoggerUtil.log(this.socket, command);
-			break;
-			
+		case "delete": handleDelete(arg); break;
+		
 		case "exit":
-			LoggerUtil.log(this.socket, command);
+			
 			this.exitSocket = true;
+			break;
+		default:
+			this.sendStringToClient("Unrecognised Command, please try again");
 			break;
 		}
 		
+	}
+	public boolean handleLs() {
+		File[] listFiles = this.currentFileDirectory.listFiles();
+		StringBuilder outputList = new StringBuilder("");
+		if(listFiles == null) {
+			return false;
+		}
+		for(File file : listFiles) {
+			outputList.append(file.getName());
+			outputList.append(file.isDirectory()? "/" : "");
+			outputList.append("\n");
+		}
+		this.sendStringToClient(outputList.toString());
+		
+		return true;
+		};
+	public boolean handleCd(String arg) {
+		if(arg.isEmpty()) {
+			this.sendStringToClient("invalid directory");
+			return false;
+		}
+		File requestedFileDirectory = new File(this.currentFileDirectory, arg).getAbsoluteFile();
+		if( requestedFileDirectory.exists() &&  requestedFileDirectory.isDirectory()) {
+			this.currentFileDirectory =  requestedFileDirectory;
+		}
+		this.sendStringToClient("changed directory to: " + arg);
+		
+		return true;};
+	public boolean handleMkDir(String name) {
+		if(name.isEmpty()) {
+			this.sendStringToClient("invalid name");
+			return false;
+		}
+		File createdFile = new File(this.currentFileDirectory, name);
+	    if (createdFile.mkdir()) {
+	    	this.sendStringToClient("Directory created: " + createdFile.getAbsolutePath());
+	    } else {
+	        this.sendStringToClient( "Failed to create directory: " + name);
+	    }
+		
+		
+		return true;};
+	public boolean handleDelete(String name) {
+		if(name.isEmpty()) {
+			this.sendStringToClient("invalid name");
+			return false;
+		}
+		File target = new File(this.currentFileDirectory, name);
+		if(!target.exists()) {
+			this.sendStringToClient( "File does not exist: " + name);
+			return false;
+		}
+	    boolean success = target.isDirectory() ? target.delete() : target.delete();
+	    if (success) {
+	        this.sendStringToClient("Deleted: " + name);
+	    } else {
+	        this.sendStringToClient("Failed to delete: " + name);
+	        return false;
+	    }
+		
+		return true;};
+	public boolean handleUpload(String arg) {return false;};
+	public boolean handleDownload(String arg) {return false;};
+	
+	public void sendStringToClient(String arg) {
+		try {
+			DataOutputStream out = new DataOutputStream(this.socket.getOutputStream());
+			out.writeUTF(arg);
+		} catch (IOException e) {
+			System.out.println("Error handling command from client# " + clientNumber + ": " + e);
+		}
 	}
 
 	public void run() {
